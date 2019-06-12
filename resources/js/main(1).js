c9c243bdfd9644d736037761d2dcae66979f45f9
Saakshaat <49038287+Saakshaat@users.noreply.github.com>
2019-06-13 01:24:19 +0530

    Adding Important Button
    Needs to be binded to local storage.

--- resources/js/main(1).js
+++ resources/js/main(1).js
@@ -1,11 +1,12 @@
-var data = (localStorage.getItem('todoList')) ? JSON.parse(localStorage.getItem('todoList')):{
+var data = (localStorage.getItem('todoList')) ? JSON.parse(localStorage.getItem('todoList')) :{
   todo: [],
-  completed: []
+  completed: [],
 };
 
 // Remove and complete icons in SVG format
 var removeSVG = '<svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 22 22" style="enable-background:new 0 0 22 22;" xml:space="preserve"><rect class="noFill" width="22" height="22"/><g><g><path class="fill" d="M16.1,3.6h-1.9V3.3c0-1.3-1-2.3-2.3-2.3h-1.7C8.9,1,7.8,2,7.8,3.3v0.2H5.9c-1.3,0-2.3,1-2.3,2.3v1.3c0,0.5,0.4,0.9,0.9,1v10.5c0,1.3,1,2.3,2.3,2.3h8.5c1.3,0,2.3-1,2.3-2.3V8.2c0.5-0.1,0.9-0.5,0.9-1V5.9C18.4,4.6,17.4,3.6,16.1,3.6z M9.1,3.3c0-0.6,0.5-1.1,1.1-1.1h1.7c0.6,0,1.1,0.5,1.1,1.1v0.2H9.1V3.3z M16.3,18.7c0,0.6-0.5,1.1-1.1,1.1H6.7c-0.6,0-1.1-0.5-1.1-1.1V8.2h10.6V18.7z M17.2,7H4.8V5.9c0-0.6,0.5-1.1,1.1-1.1h10.2c0.6,0,1.1,0.5,1.1,1.1V7z"/></g><g><g><path class="fill" d="M11,18c-0.4,0-0.6-0.3-0.6-0.6v-6.8c0-0.4,0.3-0.6,0.6-0.6s0.6,0.3,0.6,0.6v6.8C11.6,17.7,11.4,18,11,18z"/></g><g><path class="fill" d="M8,18c-0.4,0-0.6-0.3-0.6-0.6v-6.8c0-0.4,0.3-0.6,0.6-0.6c0.4,0,0.6,0.3,0.6,0.6v6.8C8.7,17.7,8.4,18,8,18z"/></g><g><path class="fill" d="M14,18c-0.4,0-0.6-0.3-0.6-0.6v-6.8c0-0.4,0.3-0.6,0.6-0.6c0.4,0,0.6,0.3,0.6,0.6v6.8C14.6,17.7,14.3,18,14,18z"/></g></g></g></svg>';
 var completeSVG = '<svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 22 22" style="enable-background:new 0 0 22 22;" xml:space="preserve"><rect y="0" class="noFill" width="22" height="22"/><g><path class="fill" d="M9.7,14.4L9.7,14.4c-0.2,0-0.4-0.1-0.5-0.2l-2.7-2.7c-0.3-0.3-0.3-0.8,0-1.1s0.8-0.3,1.1,0l2.1,2.1l4.8-4.8c0.3-0.3,0.8-0.3,1.1,0s0.3,0.8,0,1.1l-5.3,5.3C10.1,14.3,9.9,14.4,9.7,14.4z"/></g></svg>';
+var impSVG = '<svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" width="401.999px" height="401.998px" viewBox="0 0 401.999 401.998" xml:space="preserve"><rect y="0" class="noFill" width="22" height="22"/><g><g><path class="fill" d="M237.543,301.499h-73.091c-4.95,0-9.233,1.811-12.851,5.425c-3.615,3.617-5.424,7.897-5.424,12.847v63.953c0,4.948,1.809,9.232,5.424,12.854c3.621,3.61,7.9,5.421,12.851,5.421h73.097c4.942,0,9.227-1.811,12.848-5.421c3.61-3.621,5.42-7.905,5.42-12.854v-63.953c0-4.949-1.813-9.229-5.427-12.847C246.773,303.307,242.488,301.499,237.543,301.499z"/><path class="fill" d="M259.383,5.424C255.862,1.812,251.628,0,246.676,0h-91.359c-4.948,0-9.18,1.812-12.703,5.424c-3.521,3.617-5.186,7.902-4.996,12.85l7.992,219.265c0.19,4.948,2.139,9.236,5.852,12.847c3.711,3.621,8.041,5.431,12.991,5.431h73.097c4.942,0,9.271-1.81,12.991-5.431c3.71-3.61,5.653-7.898,5.852-12.847l7.987-219.265C264.578,13.326,262.905,9.045,259.383,5.424z"/></g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g></svg>';
 
 renderTodoList();
 
@@ -38,7 +39,7 @@ function renderTodoList() {
 
   for (var i = 0; i < data.todo.length; i++) {
     var value = data.todo[i];
-    addItemToDOM(value);
+    addItemToDOM(value,false);
   }
 
   for (var j = 0; j < data.completed.length; j++) {
@@ -67,7 +68,7 @@ function removeItem() {
   parent.removeChild(item);
 }
 
-function completeItem() {
+function completeItem(event) {
   var item = this.parentNode.parentNode;
   var parent = item.parentNode;
   var id = parent.id;
@@ -112,7 +113,25 @@ function addItemToDOM(text, completed) {
 
   // Add click event for completing the item
   complete.addEventListener('click', completeItem);
+  
+  var imp = document.createElement('button');
+  imp.classList.add('important');
+  imp.innerHTML = impSVG;
+  var isNotImp = true;
 
+  imp.addEventListener('click', (e)=>{
+    if(isNotImp){
+      imp.classList.replace('important','makeRed');
+      isNotImp = false;
+    }
+    else{
+      imp.classList.replace('makeRed','important');
+      isNotImp = true;
+    }
+  });
+
+  buttons.appendChild(imp);
+   
   buttons.appendChild(remove);
   buttons.appendChild(complete);
   item.appendChild(buttons);
